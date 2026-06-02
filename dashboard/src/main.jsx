f24f1b888a34e7b150371425b2e358a4478c4545
JangiliBunny <jangilibunny2@gmail.com>
2026-06-02 17:34:35 +0530

    Add CookiesProvider

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 import "./index.css";
 import Home from "./components/Home";
-import { GeneralProvider } from "./components/GeneralContext"; // ✅ ADD THIS
+import { GeneralProvider } from "./components/GeneralContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GeneralProvider> {/* ✅ WRAP HERE */}
-        <Routes>
-          <Route path="/*" element={<Home />} />
-        </Routes>
-      </GeneralProvider>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <GeneralProvider>
+          <Routes>
+            <Route path="/*" element={<Home />} />
+          </Routes>
+        </GeneralProvider>
+      </BrowserRouter>
+    </CookiesProvider>
   </React.StrictMode>
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "./index.css";
+// import Home from "./components/Home";
+// import { GeneralProvider } from "./components/GeneralContext"; // ✅ ADD THIS
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <GeneralProvider> {/* ✅ WRAP HERE */}
+//         <Routes>
+//           <Route path="/*" element={<Home />} />
+//         </Routes>
+//       </GeneralProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
