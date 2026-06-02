89647dababda1dcec5f27e3c0130fd67122f6f3b
JangiliBunny <jangilibunny2@gmail.com>
2026-06-02 17:02:08 +0530

    auth checking

--- backend/Middlewares/AuthMiddleware.js
+++ backend/Middlewares/AuthMiddleware.js
@@ -2,18 +2,50 @@ const User = require("../models/UserModel");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+
 module.exports.userVerification = (req, res) => {
-  const token = req.cookies.token
+  console.log("Cookies received:", req.cookies);
+
+  const token = req.cookies.token;
+
   if (!token) {
-    return res.json({ status: false })
+    console.log("NO TOKEN FOUND");
+    return res.json({ status: false });
   }
+
+  console.log("TOKEN FOUND");
+
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-     return res.json({ status: false })
-    } else {
-      const user = await User.findById(data.id)
-      if (user) return res.json({ status: true, user: user.username })
-      else return res.json({ status: false })
+      console.log("JWT ERROR:", err.message);
+      return res.json({ status: false });
     }
-  })
-}
+
+    const user = await User.findById(data.id);
+
+    if (user) {
+      return res.json({
+        status: true,
+        user: user.username,
+      });
+    }
+
+    return res.json({ status: false });
+  });
+};
+
+// module.exports.userVerification = (req, res) => {
+//   const token = req.cookies.token
+//   if (!token) {
+//     return res.json({ status: false })
+//   }
+//   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+//     if (err) {
+//      return res.json({ status: false })
+//     } else {
+//       const user = await User.findById(data.id)
+//       if (user) return res.json({ status: true, user: user.username })
+//       else return res.json({ status: false })
+//     }
+//   })
+// }
